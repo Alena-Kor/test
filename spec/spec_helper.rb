2d3429265b72da840e869a6e8586a805decb1dcd
Korobova Elena <ekorobova@digital-link.ru>
2022-09-06 12:16:22 +0300

    ua.txt

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,9 @@
 require_relative '../lib/core_ext/blank'
 require_relative '../lib/useragents'
 
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.syntax = [:should, :expect]
+  end
+end
